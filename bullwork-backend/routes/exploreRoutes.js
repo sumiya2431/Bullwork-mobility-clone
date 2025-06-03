81aba3eaeaa9a0778bf3e02e5d0ff1282f36840e
sumiya-31 <sumiya3131@gmail.com>
2025-06-03 18:49:09 +0530

    Initial commit for Bullwork Mobility

--- bullwork-backend/routes/exploreRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getExploreProduct } = require('../controllers/exploreController');
-
-router.get('/explore/:id', getExploreProduct);
-
-module.exports = router;
